d1a7187fd06a0925371d53096791be9ab6c2eff4
tialem112 <144235969+tialem112@users.noreply.github.com>
2024-11-22 12:50:58 +0900

    Update Runner.cs

--- Assets/01.Scripts/InGame/AIs/Runner.cs
+++ Assets/01.Scripts/InGame/AIs/Runner.cs
@@ -17,8 +17,8 @@ public class Runner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction.Normalize()
+        //direction.Normalize()
 
-        transform.Translate(direction * );
+        //transform.Translate(direction * );
     }
 }
